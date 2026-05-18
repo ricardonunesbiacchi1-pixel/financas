09705bea4ffe65de7362c5fddd44efdd7507a094
ricardonunesbiacchi1-pixel <ricardonunesbiacchi1@gmail.com>
2026-05-17 22:35:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  ],
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Finan$e',
+        short_name: 'Finan$e',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#020617',
+        theme_color: '#020617',
+
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
